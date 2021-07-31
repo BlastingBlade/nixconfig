e44af8003e793b22119811f505d40a6dcde233ab
Henry Fiantaca <hfiantaca@gmail.com>
2021-07-31 19:00:54 -0400

    Recover formatting

--- flake.nix
+++ flake.nix
@@ -11,35 +11,36 @@
     , ...
     } @ inputs: {
 
-    nixosConfigurations = let
-      modulesCommon = [
-        # Enable Flake
-        ({ pkgs, ... }: {
-          # Let 'nixos-version --json' know about the Git revision of this flake.
-          system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
 
-          nix.package = pkgs.nixUnstable;
-          nix.extraOptions = ''
-            experimental-features = nix-command flakes
-          '';
-        })
-        ./hosts/common.nix
-      ];
-    in {
-      oldbook = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = modulesCommon ++ [
-          ./hosts/desktop.nix
-          ./hosts/oldbook.nix
-          nixos-hardware.nixosModules.common-gpu-nvidia-disable
-          nixos-hardware.nixosModules.dell-latitude-3480
+      nixosConfigurations = let
+        modulesCommon = [
+          # Enable Flake
           ({ pkgs, ... }: {
-            hardware.opengl.enable = true;
-            hardware.bluetooth.enable = true;
+            # Let 'nixos-version --json' know about the Git revision of this flake.
+            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+
+            nix.package = pkgs.nixUnstable;
+            nix.extraOptions = ''
+              experimental-features = nix-command flakes
+            '';
           })
+          ./hosts/common.nix
         ];
-        specialArgs = { inherit inputs; };
+      in {
+        oldbook = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = modulesCommon ++ [
+            ./hosts/desktop.nix
+            ./hosts/oldbook.nix
+            nixos-hardware.nixosModules.common-gpu-nvidia-disable
+            nixos-hardware.nixosModules.dell-latitude-3480
+            ({ pkgs, ... }: {
+              hardware.opengl.enable = true;
+              hardware.bluetooth.enable = true;
+            })
+          ];
+          specialArgs = { inherit inputs; };
+        };
       };
     };
-  };
 }
