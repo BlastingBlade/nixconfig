61e5a5eef552b7af075e615abb35643772cd7adc
Henry Fiantaca <hfiantaca@gmail.com>
2021-09-10 19:56:48 -0400

    Reformat mu4e config, and start with use-package!

--- home/doom.d/config.el
+++ home/doom.d/config.el
@@ -63,43 +63,56 @@
   linkcolor=black}
   "))
 
-(setq +mu4e-mu4e-path "~/.local/share/maildir/"
-      send-mail-function #'smtpmail-send-it
-      message-sendmail-f-is-evil t
-      message-sendmail-extra-arguments '("--read-envelope-from")
-      message-send-mail-function #'message-send-mail-with-sendmail)
+(use-package! mu4e
+  ; forced loading by ommiting :defer, :hook, :commands, and :after
+  :init
+  (setq +mu4e-mu4e-path "~/.local/share/maildir/"
+        send-mail-function #'smtpmail-send-it
+        message-sendmail-f-is-evil t
+        message-sendmail-extra-arguments '("--read-envelope-from")
+        message-send-mail-function #'message-send-mail-with-sendmail
+        +mu4e-gmail-accounts '(("hfiantaca@gmail.com" . "/gmail")
+                               ("hfiantac@uncc.edu" . "/uncc"))
+        mu4e-index-cleanup nil
+        mu4e-index-lazy-check t
+        mu4e-update-interval (* 60 60))
 
-(set-email-account! "gmail"
-  '((user-mail-address      . "hfiantaca@gmail.com")
-    (mu4e-compose-signature . "---\nHenry Fiantaca")
+  (set-email-account! "gmail"
+    '((user-mail-address      . "hfiantaca@gmail.com")
+      (mu4e-compose-signature . "---\nHenry Fiantaca")
 
-    (mu4e-sent-folder   . "/gmail/[Gmail]/Sent Mail")
-    (mu4e-drafts-folder . "/gmail/[Gmail]/Drafts")
-    (mu4e-trash-folder  . "/gmail/[Gmail]/Trash")
-    (mu4e-refile-folder . "/gmail/[Gmail]/All Mail")
+      (mu4e-sent-folder   . "/gmail/[Gmail]/Sent Mail")
+      (mu4e-drafts-folder . "/gmail/[Gmail]/Drafts")
+      (mu4e-trash-folder  . "/gmail/[Gmail]/Trash")
+      (mu4e-refile-folder . "/gmail/[Gmail]/All Mail")
 
-    (mu4e-sent-messages-behavior . delete)
-    (mu4e-index-cleanup    . nil)
-    (mu4e-index-lazy-check . t)
+      (mu4e-sent-messages-behavior . delete)
+      (mu4e-index-cleanup    . nil)
+      (mu4e-index-lazy-check . t)
 
-    (smtpmail-smtp-user    . "hfiantaca@gmail.com")
-    (smtpmail-smtp-server  . "smtp.gmail.com")
-    (smtpmail-smtp-service . 465))
-  t)
+      (smtpmail-smtp-user    . "hfiantaca@gmail.com")
+      (smtpmail-smtp-server  . "smtp.gmail.com")
+      (smtpmail-smtp-service . 465))
+    t)
 
-(set-email-account! "uncc"
-  '((user-mail-address      . "hfiantac@uncc.edu")
-    (mu4e-compose-signature . "---\nHenry Fiantaca\n801075065")
+  (set-email-account! "uncc"
+    '((user-mail-address      . "hfiantac@uncc.edu")
+      (mu4e-compose-signature . "---\nHenry Fiantaca\n801075065")
 
-    (mu4e-sent-folder   . "/uncc/[Gmail]/Sent Mail")
-    (mu4e-drafts-folder . "/uncc/[Gmail]/Drafts")
-    (mu4e-trash-folder  . "/uncc/[Gmail]/Trash")
-    (mu4e-refile-folder . "/uncc/[Gmail]/All Mail")
+      (mu4e-sent-folder   . "/uncc/[Gmail]/Sent Mail")
+      (mu4e-drafts-folder . "/uncc/[Gmail]/Drafts")
+      (mu4e-trash-folder  . "/uncc/[Gmail]/Trash")
+      (mu4e-refile-folder . "/uncc/[Gmail]/All Mail")
 
-    (mu4e-sent-messages-behavior . delete)
-    (mu4e-index-cleanup    . nil)
-    (mu4e-index-lazy-check . t)
+      (mu4e-sent-messages-behavior . delete)
+      (mu4e-index-cleanup    . nil)
+      (mu4e-index-lazy-check . t)
 
-    (smtpmail-smtp-user    . "hfiantac@uncc.edu")
-    (smtpmail-smtp-server  . "smtp.gmail.com")
-    (smtpmail-smtp-service . 465)))
+      (smtpmail-smtp-user    . "hfiantac@uncc.edu")
+      (smtpmail-smtp-server  . "smtp.gmail.com")
+      (smtpmail-smtp-service . 465)))
+  :config
+  ; start the mu4e backend
+  (mu4e t))
+
+;; nix-doom-emacs has not installed mu4e-alert, else it would be enabled here
