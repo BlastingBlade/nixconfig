2d1e2b035f1c55cf564e4e8636fc4d59975c2f9d
Henry Fiantaca <hfiantaca@gmail.com>
2021-09-16 12:22:07 -0400

    Use module configuration to persist openssh keys

--- hosts/histoire.nix
+++ hosts/histoire.nix
@@ -11,16 +11,18 @@
       "/home"      # user dirs
     ];
   };
-  environment.etc."ssh/ssh_host_rsa_key".source
-    = "/nix/persist/etc/ssh/ssh_host_rsa_key";
-  environment.etc."ssh/ssh_host_rsa_key.pub".source
-    = "/nix/persist/etc/ssh/ssh_host_rsa_key.pub";
-  environment.etc."ssh/ssh_host_ed25519_key".source
-    = "/nix/persist/etc/ssh/ssh_host_ed25519_key";
-  environment.etc."ssh/ssh_host_ed25519_key.pub".source
-    = "/nix/persist/etc/ssh/ssh_host_ed25519_key.pub";
   environment.etc."machine-id".source
     = "/nix/persist/etc/machine-id";
+
+  services.openssh.hostKeys = [
+    { bits = 4096;
+      path = "/nix/persist/etc/ssh/ssh_host_rsa_key";
+      type = "rsa";
+    }
+    { path = "/nix/persist/etc/ssh/ssh_host_ed25519_key";
+      type = "ed25519";
+    }
+  ];
 
   environment.systemPackages = with pkgs; [
     inetutils
