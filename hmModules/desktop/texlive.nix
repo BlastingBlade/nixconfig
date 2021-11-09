39761dc78c80b3b9335bb568a1253aa351e7cf04
Henry Fiantaca <hfiantaca@gmail.com>
2021-11-09 14:21:20 -0500

    Install full texlive scheme
    I give up trying to select the right packages, take my HDD space

--- hmModules/desktop/texlive.nix
+++ hmModules/desktop/texlive.nix
@@ -6,18 +6,7 @@
     packageSet = pkgs.texlive;
     extraPackages = tpkgs: {
       inherit (tpkgs)
-        scheme-medium
-        collection-fontsrecommended
-        wrapfig
-        minted
-        fvextra
-        upquote
-        catchfile
-        xstring
-        framed
-        titling
-        paralist
-        forest;
+        scheme-full;
     };
   };
 }
